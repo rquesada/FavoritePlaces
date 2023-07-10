0384d391fc5183cdbcd71136026ebef140f13f43
Roy Quesada <roy.quesada@gmail.com>
2023-07-10 14:57:18 -0600

    Use expo-image-picker to take pictures

---
+++ screens/AddPlace.js
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import PlaceForm from "../components/Places/PlaceForm";
+
+function AddPlace(){
+    return (
+        <PlaceForm />
+    );
+}
+
+export default AddPlace;
