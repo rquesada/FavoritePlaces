2dc94658f84602ffaf69afaebb0c7099cf4919d8
Roy Quesada <roy.quesada@gmail.com>
2023-07-10 18:21:19 -0600

    Add Pin in the map

--- screens/Map.js
+++ screens/Map.js
@@ -1,15 +1,40 @@
 import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet } from 'react-native'; 
+import { useState } from 'react';
 
 function Map(){
+
+    const [selectedLocation, setSelectedLocation] = useState();
+
     const region = {
         latitude: 37.78,
         longitude: -122.43,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     };
+
+    function selectLocationHandler(event){
+        //console.log(event)
+        const lat = event.nativeEvent.coordinate.latitude;
+        const lng = event.nativeEvent.coordinate.longitude;
+        console.log(event.nativeEvent.coordinate.latitude)
+        setSelectedLocation({lat: lat, lng: lng});
+    }
+
     return(
-        <MapView style={styles.map} initialRegion={region}></MapView>
+        <MapView 
+            style={styles.map} 
+            initialRegion={region}
+            onPress={selectLocationHandler}
+        >    
+            {selectedLocation && <Marker 
+                title='Picked Location'
+                coordinate={{ 
+                    latitude: selectedLocation.lat, 
+                    longitude: selectedLocation.lng 
+                }} 
+            />}
+        </MapView>
     );
 }
 
