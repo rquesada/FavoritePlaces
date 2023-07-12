83978bb0bc71009ed2bbc70ca2aa0e8a66a27b81
Roy Quesada <roy.quesada@gmail.com>
2023-07-11 19:22:20 -0600

    Fetch a place by id

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import Map from './screens/Map';
 import { useEffect, useState } from 'react';
 import { init } from './util/database';
 import AppLoading from 'expo-app-loading';
+import PlaceDetails from './screens/PlaceDetails';
 
 const Stack = createNativeStackNavigator();
 
@@ -61,6 +62,9 @@ export default function App() {
             name='Map' 
             component={Map}
           />
+          <Stack.Screen name="PlaceDetails" component={PlaceDetails} options={
+            { title: 'Loading Place...'}
+          }/>
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -7,26 +7,45 @@ import {
 } from "react-native";
 import OutlinedButton from "../components/ui/OutlinedButton";
 import { Colors } from "../constants/colors";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { fetchPlaceDetails } from "../util/database";
 
-function PlaceDetails({route}){
+function PlaceDetails({route, navigation}){
 
+    const [fetchedPlace, setFetchedPlace] = useState();
+    
     function showOnMapHandler(){
-
+        navigation.navigate('Map');
     }
 
     const selectedPlaceId = route.params.placeId;
 
     useEffect(()=>{
+        async function loadPlaceData(){
+            const place = await fetchPlaceDetails(selectedPlaceId);
+            setFetchedPlace(place);
+            navigation.setOptions({
+                title: place.title,
+            });
+        }
 
+        loadPlaceData();
     }, [selectedPlaceId]);
+
+    if(!fetchedPlace){
+        return(
+            <View style={styles.fallback}>
+                <Text>Loading place data...</Text>
+            </View>
+        );
+    }
 
     return (
         <ScrollView>
-            <Image style={styles.image} />
+            <Image style={styles.image} source={{uri: fetchedPlace.imageUri}} />
             <View style={styles.locationContainer}>
                 <View style={styles.addressContainer}>
-                    <Text style={styles.address}>ADDRESS</Text>
+                    <Text style={styles.address}>{fetchedPlace.address}</Text>
                 </View>
                 <OutlinedButton icon="map" onPress={showOnMapHandler}>View on Map</OutlinedButton>
             </View>
@@ -38,6 +57,11 @@ function PlaceDetails({route}){
 export default PlaceDetails;
 
 const styles = StyleSheet.create({
+    fallback:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     image:{
         height: '35%',
         minHeight: 300,
